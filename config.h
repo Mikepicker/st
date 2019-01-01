f3e6555e3704bc241e86d6445c72dbd27507475c
Mikepicker <michele.rullo90@gmail.com>
2019-01-01 17:21:58 +0100

    [+] fira mono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:pixelsize=12:antialias=true:autohint=true:regular";
+static char *font = "Fira Mono Medium:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
